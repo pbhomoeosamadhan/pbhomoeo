0efbac3bee0d44bf67207c6d25e80c40836bbadc
MostafaHakim <mdmostafahakim@gmail.com>
2025-12-24 17:33:55 +0600

    first commit

--- server/app.js
+++ server/app.js
@@ -4,7 +4,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 
-app.use(cors("*"));
+app.use(
+  cors({
+    origin: ["*", "https://pbhms.vercel.app/*"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 5000;
